60fa9668ef2efa27bc90b7880f006f1757c71d7d
myrrhex06 <gitlwer210@gmail.com>
2026-06-07 19:02:06 +0900

    Initial commit

---
+++ src/main/java/com/earth_chat/EarthChatApplication.java
@@ -0,0 +1,13 @@
+package com.earth_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/earth_chat/EarthChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earth_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
