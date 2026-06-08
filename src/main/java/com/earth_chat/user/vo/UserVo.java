178f1d589dcf72966d738b51abf4ae11b21e0e8f
myrrhex06 <gitlwer210@gmail.com>
2026-06-08 21:55:20 +0900

    feat: 사용자 테이블 VO 객체 정의

---
+++ src/main/java/com/earth_chat/user/vo/UserVo.java
@@ -0,0 +1,43 @@
+package com.earth_chat.user.vo;
+
+import com.earth_chat.common.enums.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserVo {
+
+    // 사용자 SEQ
+    private Long userSeq;
+
+    // 닉네임
+    private String nickname;
+
+    // 이메일
+    private String email;
+
+    // 비밀번호
+    private String pwd;
+
+    // 사용 여부
+    private String useYn;
+
+    // 번역 코드
+    private String translateCode;
+
+    // 계정 상태
+    private UserStatus userStatus;
+
+    // 등록일시
+    private LocalDateTime regDt;
+
+    // 수정일시
+    private LocalDateTime modDt;
+}
