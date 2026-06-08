729981b4e69c318a5cb3e3fe86314d14204fd108
myrrhex06 <gitlwer210@gmail.com>
2026-06-09 06:47:24 +0900

    feat: 이미 존재하는 이메일, 닉네임일 경우 발생시킬 예외 정의

---
+++ src/main/java/com/earth_chat/common/exception/AlreadyExistsEmailException.java
@@ -0,0 +1,8 @@
+package com.earth_chat.common.exception;
+
+public class AlreadyExistsEmailException extends RuntimeException {
+
+    public AlreadyExistsEmailException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/earth_chat/common/exception/AlreadyExistsNicknameException.java
@@ -0,0 +1,8 @@
+package com.earth_chat.common.exception;
+
+public class AlreadyExistsNicknameException extends RuntimeException {
+
+    public AlreadyExistsNicknameException(String message) {
+        super(message);
+    }
+}
