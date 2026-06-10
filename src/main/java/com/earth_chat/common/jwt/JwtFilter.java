874c0cc45ec716d2ab280bf6ed245e1a544b92b5
myrrhex06 <gitlwer210@gmail.com>
2026-06-11 06:43:44 +0900

    feat: JWT 토큰 검증 Filter 구현

---
+++ src/main/java/com/earth_chat/common/jwt/JwtFilter.java
@@ -0,0 +1,57 @@
+package com.earth_chat.common.jwt;
+
+import com.earth_chat.common.custom.CustomUserDetails;
+import com.earth_chat.user.service.UserService;
+import com.earth_chat.user.vo.RoleVo;
+import com.earth_chat.user.vo.UserVo;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+        String token = null;
+
+        if (authorization != null && authorization.startsWith("Bearer ")) {
+            token = authorization.substring(7);
+        }
+
+        if(token != null && jwtTokenProvider.validateToken(token)){
+
+            Claims claims = jwtTokenProvider.parseToken(token);
+
+            List<SimpleGrantedAuthority> auth = jwtTokenProvider.getAuthorities(claims);
+
+            UserVo user = userService.selectUserByEmail(claims.getSubject());
+            List<RoleVo> roleList = userService.selectRolesByUserSeq(user.getUserSeq());
+            user.setRoleList(roleList);
+
+            CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(customUserDetails, null, auth);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
