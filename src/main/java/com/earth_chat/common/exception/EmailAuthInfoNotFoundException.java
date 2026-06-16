77118c57c3ca3197613288be09120a8b7648ded0
myrrhex06 <gitlwer210@gmail.com>
2026-06-16 20:44:59 +0900

    feat: 이메일 인증 정보를 찾지 못했을 경우 발생

---
+++ src/main/java/com/earth_chat/common/exception/EmailAuthInfoNotFoundException.java
@@ -0,0 +1,8 @@
+package com.earth_chat.common.exception;
+
+public class EmailAuthInfoNotFoundException extends RuntimeException {
+
+    public EmailAuthInfoNotFoundException(String message) {
+        super(message);
+    }
+}
