5374f1568569a33e47abe877b600c67d5acbcd9d
myrrhex06 <gitlwer210@gmail.com>
2026-06-22 06:37:05 +0900

    feat: 토큰 재발급 요청 객체 정의

---
+++ src/main/java/com/earth_chat/auth/controller/request/TokenRefreshRequest.java
@@ -0,0 +1,15 @@
+package com.earth_chat.auth.controller.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRefreshRequest {
+
+    @Schema(description = "Refresh Token", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String token;
+}
