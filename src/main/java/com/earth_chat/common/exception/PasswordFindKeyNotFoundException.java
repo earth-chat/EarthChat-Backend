148eb71ee906bc13da239eb58f4a1c101e13b95f
myrrhex06 <gitlwer210@gmail.com>
2026-06-22 06:35:16 +0900

    feat: 비밀번호 찾기 키를 찾지 못했을 경우 발생시킬 예외 정의

---
+++ src/main/java/com/earth_chat/common/exception/PasswordFindKeyNotFoundException.java
@@ -0,0 +1,8 @@
+package com.earth_chat.common.exception;
+
+public class PasswordFindKeyNotFoundException extends RuntimeException {
+
+    public PasswordFindKeyNotFoundException(String message) {
+        super(message);
+    }
+}
