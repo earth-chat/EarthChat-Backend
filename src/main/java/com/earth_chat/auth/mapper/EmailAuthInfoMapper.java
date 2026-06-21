42535ba2a97d1b7e1ba31e7d4f83dd60571cd197
myrrhex06 <gitlwer210@gmail.com>
2026-06-21 11:30:22 +0900

    feat: 인증 완료된 정보 존재 여부 확인 쿼리 정의 및 주석 작성

--- src/main/java/com/earth_chat/auth/mapper/EmailAuthInfoMapper.java
+++ src/main/java/com/earth_chat/auth/mapper/EmailAuthInfoMapper.java
@@ -6,9 +6,31 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface EmailAuthInfoMapper {
 
+    /**
+     * 이메일 인증 정보 삽입.
+     * @param emailAuthInfoVo 이메일 인증 정보
+     * @return int
+     */
     int insertEmailAuthInfo(EmailAuthInfoVo emailAuthInfoVo);
 
+    /**
+     * 이메일 인증 정보 조회.
+     * @param email 이메일
+     * @return EmailAuthInfoVo
+     */
     EmailAuthInfoVo selectEmailAuthInfoByEmail(String email);
 
+    /**
+     * 이메일 인증 정보 업데이트.
+     * @param authInfo 이메일 인증 정보
+     * @return int
+     */
     int updateEmailAuthInfo(EmailAuthInfoVo authInfo);
+
+    /**
+     * 인증이 완료된 정보가 존재하는지 확인.
+     * @param email 이메일
+     * @return boolean
+     */
+    boolean existsAuthedInfoByEmail(String email);
 }
