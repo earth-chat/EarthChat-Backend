465f7f9adcbed4d9fc20ae107662c4f260def383
myrrhex06 <gitlwer210@gmail.com>
2026-06-28 19:42:04 +0900

    feat: i18n 메시지 코드 enum 정의

---
+++ src/main/java/com/earth_chat/common/enums/MessageCode.java
@@ -0,0 +1,36 @@
+package com.earth_chat.common.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum MessageCode {
+
+    // 사용자 관련 Message Code
+    USER_NOT_FOUND("user.not-found"),
+    USER_DUPLICATE_EMAIL("user.duplicate-email"),
+    USER_DUPLICATE_NICKNAME("user.duplicate-nickname"),
+    USER_INVALID_PASSWORD("user.invalid-password"),
+    USER_REQUIRED_EMAIL_AUTH("user.require-email-auth"),
+
+    // 인증코드 관련 Message Code
+    AUTH_CODE_INVALID("auth-code.invalid"),
+    AUTH_CODE_EXPIRED("auth-code.expired"),
+
+    // 비밀번호 찾ㄱ기 키 관련 Message Code
+    PASSWORD_KEY_INVALID("password-key.invalid"),
+    PASSWORD_KEY_EXPIRED("password-key.expired"),
+
+    // Refresh Token 관련 Message Code
+    REFRESH_TOKEN_NOT_FOUND("refresh-token.not-found"),
+    REFRESH_TOKEN_EXPIRED("refresh-token.expired"),
+
+    // 인증 관련 Message Code
+    AUTH_UNAUTHORIZED("auth.unauthorized"),
+
+    // 서버 Message Code
+    INTERNAL_SERVER_ERROR("server.internal-server-error");
+
+    private final String code;
+}
