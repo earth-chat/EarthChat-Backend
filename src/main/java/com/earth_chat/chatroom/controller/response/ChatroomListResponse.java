29898d2910824f7c152e339449e8756151de5049
myrrhex06 <gitlwer210@gmail.com>
2026-07-04 22:28:20 +0900

    feat: 채팅방 목록 응답 객체 정의

---
+++ src/main/java/com/earth_chat/chatroom/controller/response/ChatroomListResponse.java
@@ -0,0 +1,43 @@
+package com.earth_chat.chatroom.controller.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatroomListResponse {
+
+    @Schema(description = "채팅방 SEQ")
+    private Long chatroomSeq;
+
+    @Schema(description = "채팅방명")
+    private String name;
+
+    @Schema(description = "설명")
+    private String description;
+
+    @Schema(description = "방장 SEQ")
+    private Long ownerSeq;
+
+    @Schema(description = "방장 닉네임")
+    private String ownerNickname;
+
+    @Schema(description = "최대 참여 가능 인원")
+    private Integer maxParticipantNum;
+
+    @Schema(description = "공개 여부")
+    private String publicYn;
+
+    @Schema(description = "등록일시")
+    private LocalDateTime regDt;
+
+    @Schema(description = "수정일시")
+    private LocalDateTime modDt;
+}
