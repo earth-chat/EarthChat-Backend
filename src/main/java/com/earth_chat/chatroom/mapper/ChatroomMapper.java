3c6944ca7e02be0121c6129f8475648fdb4b1d7e
myrrhex06 <gitlwer210@gmail.com>
2026-07-05 19:39:32 +0900

    feat: 채팅방 정보 삽입, 페이징 조회 쿼리 작성

---
+++ src/main/java/com/earth_chat/chatroom/mapper/ChatroomMapper.java
@@ -0,0 +1,33 @@
+package com.earth_chat.chatroom.mapper;
+
+import com.earth_chat.chatroom.vo.ChatroomVo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ChatroomMapper {
+
+    /**
+     * 채팅방 INSERT.
+     * @param chatroomVo 채팅방 정보
+     * @return int
+     */
+    int insertChatroom(ChatroomVo chatroomVo);
+
+    /**
+     * 채팅방 목록 조회.
+     * @param page 페이지 번호
+     * @param size 데이터 개수
+     * @return List<ChatroomVo>
+     */
+    List<ChatroomVo> selectChatroomAsPagination(int page, int size);
+
+    /**
+     * 채팅방 목록 전체 데이터 개수 조회.
+     * @param page 페이지 번호
+     * @param size 데이터 개수
+     * @return int
+     */
+    long selectTotalCountAsPagination(int page, int size);
+}
