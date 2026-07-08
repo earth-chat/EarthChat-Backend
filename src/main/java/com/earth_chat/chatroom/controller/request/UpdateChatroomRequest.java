41f2f28152b9764cfe2b4530eb662021773d9683
myrrhex06 <gitlwer210@gmail.com>
2026-07-08 17:38:21 +0900

    fix: 채팅방 정보 수정 요청, 응답 객체 정의

---
+++ src/main/java/com/earth_chat/chatroom/controller/request/UpdateChatroomRequest.java
@@ -0,0 +1,30 @@
+package com.earth_chat.chatroom.controller.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateChatroomRequest {
+
+    @Schema(description = "채팅방 SEQ", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long chatroomSeq;
+
+    @Schema(description = "채팅방명", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String name;
+
+    @Schema(description = "설명", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String description;
+
+    @Schema(description = "최대 참여 가능 인원", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private Integer maxParticipantNum;
+
+    @Schema(description = "공개 여부", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String publicYn;
+
+    @Schema(description = "채팅방 비밀번호", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    private String roomPwd;
+}
---
+++ src/main/java/com/earth_chat/chatroom/controller/response/UpdateChatroomResponse.java
@@ -0,0 +1,49 @@
+package com.earth_chat.chatroom.controller.response;
+
+import com.earth_chat.chatroom.vo.ChatroomVo;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateChatroomResponse {
+
+    @Schema(description = "채팅방 SEQ")
+    private Long chatroomSeq;
+
+    @Schema(description = "채팅방명")
+    private String name;
+
+    @Schema(description = "설명")
+    private String description;
+
+    @Schema(description = "방장 SEQ")
+    private Long ownerSeq;
+
+    @Schema(description = "최대 참여 가능 인원")
+    private Integer maxParticipantNum;
+
+    @Schema(description = "공개 여부")
+    private String publicYn;
+
+    /**
+     * 응답 객체 생성.
+     * @param chatroomVo 채팅방 정보
+     * @return UpdateChatroomResponse
+     */
+    public static UpdateChatroomResponse of(ChatroomVo chatroomVo) {
+        return UpdateChatroomResponse.builder()
+                .chatroomSeq(chatroomVo.getChatroomSeq())
+                .name(chatroomVo.getChatroomName())
+                .description(chatroomVo.getChatroomDescription())
+                .ownerSeq(chatroomVo.getOwnerSeq())
+                .maxParticipantNum(chatroomVo.getMaxParticipantNum())
+                .publicYn(chatroomVo.getPublicYn())
+                .build();
+    }
+}
